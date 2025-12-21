c0d7b1cb805eafd23e6d9898e67f42f1be9bca57
Marbobley <fournier.wilf@gmail.com>
2025-12-21 10:37:39 +0100

    Add new entity

---
+++ src/Entity/VisitorInformation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VisitorInformationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Nora\EntitiesVisitorBundle\Model\VisitorInformation as VisitorInformationModel;
+
+#[ORM\Entity(repositoryClass: VisitorInformationRepository::class)]
+class VisitorInformation extends VisitorInformationModel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
