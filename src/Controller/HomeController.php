d641f52b03bd9ca299b8f95a6b2c518c134c2177
Marbobley <fournier.wilf@gmail.com>
2025-12-29 16:13:46 +0100

    cleanign

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\VisitorInformation;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
