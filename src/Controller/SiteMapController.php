4d742d0ad74596f740b8b2e98b07a438adc5024e
Marbobley <fournier.wilf@gmail.com>
2026-01-19 19:45:00 +0100

    amelioration SEO

--- src/Controller/SiteMapController.php
+++ src/Controller/SiteMapController.php
@@ -8,16 +8,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class SiteMapController extends AbstractController
 {
     #[Route("sitemap.xml", name: 'sitemap', format: 'xml')]
     public function index(): Response
     {
         $urls = [];
-        $urls[] = ['loc' => $this->generateUrl('app_home')];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_home', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'lastmod' => date('Y-m-d'),
+            'changefreq' => 'weekly',
+            'priority' => '1.0'
+        ];
 
-        return $this->render('sitemap/sitemap.xml.twig', [
-            'urls' => $urls,
-        ]);
+        $response = new Response(
+            $this->renderView('sitemap/sitemap.xml.twig', [
+                'urls' => $urls,
+            ]),
+            Response::HTTP_OK
+        );
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
     }
 }
